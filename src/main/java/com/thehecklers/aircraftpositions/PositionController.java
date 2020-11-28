d38500da90f16d7c34a22a5c9a9c534d7f15e5b6
Mark Heckler <mark.heckler@gmail.com>
2020-11-28 13:37:29 -0600

    Point in time commit

--- src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -1,38 +1,78 @@
 package com.thehecklers.aircraftpositions;
 
+import org.reactivestreams.Publisher;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Controller
 public class PositionController {
-    private final AircraftRepository repository;
+    //    private final AircraftRepository repository;
+    private final PositionService service;
     private final RSocketRequester requester;
-    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
+//    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
 
 
-    public PositionController(AircraftRepository repository, RSocketRequester.Builder builder) {
-        this.repository = repository;
-        this.requester = builder.connectTcp("localhost", 7635).block();
+    //public PositionController(AircraftRepository repository, RSocketRequester.Builder builder) {
+    //public PositionController(PositionService service, RSocketRequester.Builder builder) {
+    public PositionController(PositionService service, RSocketRequester requester) {
+//        this.repository = repository;
+        this.service = service;
+        //this.requester = builder.connectTcp("localhost", 7635).block();
+        this.requester = requester;
     }
+
+//    @GetMapping("/aircraft")
+//    public String getCurrentAircraftPositions(Model model) {
+//        Flux<Aircraft> aircraftFlux = repository.deleteAll()
+//                .thenMany(client.get()
+//                        .retrieve()
+//                        .bodyToFlux(Aircraft.class)
+//                        .filter(plane -> !plane.getReg().isEmpty())
+//                        .flatMap(repository::save));
+//
+//        model.addAttribute("currentPositions", aircraftFlux);
+//        return "positions";
+//    }
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
-        Flux<Aircraft> aircraftFlux = repository.deleteAll()
-                .thenMany(client.get()
-                        .retrieve()
-                        .bodyToFlux(Aircraft.class)
-                        .filter(plane -> !plane.getReg().isEmpty())
-                        .flatMap(repository::save));
+        model.addAttribute("currentPositions", service.getAllAircraft());
 
-        model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
     }
+
+/*
+    @ResponseBody
+    @GetMapping("/acpos")
+    public Flux<Aircraft> getCurrentACPositions() {
+        return service.getAllAircraft();
+    }
+
+    @ResponseBody
+    @GetMapping("/acpos/search")
+    public Publisher<Aircraft> searchForACPosition(@RequestParam Map<String, String> searchParams) {
+        if (!searchParams.isEmpty()) {
+            Map.Entry<String, String> setToSearch = searchParams.entrySet().iterator().next();
+
+            if (setToSearch.getKey().equalsIgnoreCase("id")) {
+                return service.getAircraftById(Long.valueOf(setToSearch.getValue()));
+            } else {
+                return service.getAircraftByReg(setToSearch.getValue());
+            }
+        } else {
+            return Mono.empty();
+        }
+    }
+*/
 
     @ResponseBody
     @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
